6a712b70f448da6a72c5ab581fb1f1e19c69d53a
Hubot <nishihaider@yahoo.com>
2020-08-26 09:40:08 -0400

    dun

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./images/logo.jpeg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul className="Navigation-container">
+        <li className="Navigation-list">
+          <a href="./App.js"> To Do</a>
+        </li>
+        <li className="Navigation-list">
+          <a href="./App.js">Calendar</a>
+        </li>
+        <li className="Navigation-list">
+          <a href="./App.js">Notes</a>
+        </li>
+      </ul>
+      <img src={logo} className="App-avatar-logo" alt="Avatar-logo" />
+      <h2>Nisha</h2>
+      <h1>To Do Today</h1>
+      <div className="Todo-list">
+        <div>
+          <form className="Todo-button">
+            <button type="submit">Done</button>
+            <input placeholder="Add your To Do here"></input>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
+
+// function AppEdit() {
+//   return (
+//     <>
+//       <ul>
+//         <li>
+//           <a href="">To Do</a>
+//         </li>
+//         <li>
+//           <a href="">Calendar</a>
+//         </li>
+//         <li>
+//           <a href="">Notes</a>
+//         </li>
+//       </ul>
+//       <img src={logo} className="App-avatar-logo" alt="Avatar-logo" />
+//       <h2>Nisha</h2>
+//       <h1>Edit</h1>
+//       <div className="Todo-edit">
+//         <form>
+//           <input placeholder="Title"></input>
+//           <input placeholder="Description"></input>
+//           <input placeholder="Add image"></input>
+//           <button type="submit">Save</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+/* <form className="Todo-button">
+          <button type="submit">Done</button>
+          <input placeholder="Add your To Do here"></input>
+        </form> */
 
 export default App;
