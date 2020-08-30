fdd098106696d06097d06ac2dbc1e84d4271bc5d
Hubot <nishihaider@yahoo.com>
2020-08-29 23:12:31 -0400

    reorganized the CSS formats

--- src/App.js
+++ src/App.js
@@ -1,200 +1,174 @@
 import React from "react";
-import jassy from "./images/jassy.jpeg";
+import Image from "./images/jassy.jpeg";
 import "./App.css";
-
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-// } from "react-router-dom";
-
-// Install the Styled Components link below - ask for help
-
-// const Button = styled.a`
-//   display: inline-block;
-//   border-radius: 3px;
-//   padding: 0.5rem 0;
-//   margin: 0.5rem 1rem;
-//   width: 11rem;
-//   background: transparent;
-//   color: white;
-//   border: 2px solid white;
-
-//   ${(props) =>
-//     props.primary &&
-//     css`
-//       background: white;
-//       color: black;
-//     `}
-// `;
-
-// render(
-//   <div>
-//     <Button
-//       href="https://github.com/styled-components/styled-components"
-//       target="_blank"
-//       rel="noopener"
-//       primary
-//     >
-//       GitHub
-//     </Button>
-
-//     <Button as={Link} href="/docs">
-//       Documentation
-//     </Button>
-//   </div>
-// );
 
 const todos = [
   {
-    todo: "Study react",
     id: 1,
-    todoDone: true,
+    todo: "Study react",
+    isCompleted: true,
   },
   {
-    todo: "Buy Lotto ticket",
     id: 2,
-    todoDone: true,
+    todo: "Buy Lotto ticket",
+    isCompleted: false,
   },
   {
-    todo: "Buy car",
     id: 3,
-    todoDone: true,
+    todo: "Buy car",
+    isCompleted: false,
   },
 ];
+const user = {
+  name: "Jassy",
+  image: Image,
+};
 
 function App() {
   return (
     <>
-      <header style={HeaderStyles}>
-        <ul style={NavStyle}>
-          <li style={NavItemStyle}>
-            <a style={NavLink} href="./App.js">
+      <header style={navStyle.headerContainer}>
+        <ul style={navStyle.listContainer}>
+          <li style={navStyle.listItems}>
+            <a style={navStyle.listLink} href="./App.js">
               To Do
             </a>
           </li>
-          <li style={NavItemStyle}>
-            <a style={NavLink} href="./App.js">
+          <li style={navStyle.listItems}>
+            <a style={navStyle.listLink} href="./App.js">
               Calendar
             </a>
           </li>
-          <li style={NavItemStyle}>
-            <a style={NavLink} href="./App.js">
+          <li style={navStyle.listItems}>
+            <a style={navStyle.listLink} href="./App.js">
               Notes
             </a>
           </li>
         </ul>
       </header>
-      <div style={userAvatarContainer}>
-        <img style={avatar} src={jassy} alt="user-avatar" />
-        <h2>Nisha</h2>
-        <h1>To Do Today</h1>
+      <div style={userAvatarContainer.avatarContainer}>
+        <img
+          style={userAvatarContainer.avatarImage}
+          src={user.image}
+          alt="user-avatar"
+        />
+        <h2>{user.name}</h2>
       </div>
-      <div style={todoList}>
-        <ul>
-          {todos.map((todo) => {
-            return (
-              <li style={listItemStyle}>
-                <p>{todo.todo}</p>
-                <input type="checkbox" checked={todo.todoDone} />
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div style={submitButton}>
-        <form style={AddToDoButton}>
-          <button style={InputDoneButton} type="submit">
-            Done
-          </button>
-          <input style={InputButton} placeholder="Add your To Do here"></input>
-          <button style={InputDeleteButton} type="submit">
-            Delete
-          </button>
-        </form>
-      </div>
+      <h1 style={listItemStyle.titleList}>To Do Today</h1>
+      <ul style={listItemStyle.listContainer}>
+        {todos.map((todoItem) => (
+          <li style={listItemStyle.listItem}>
+            <input
+              style={listItemStyle.inputTodo}
+              type="checkbox"
+              checked={todoItem.isCompleted}
+            />
+            <span style={listItemStyle.listItemtodo}>{todoItem.todo}</span>
+          </li>
+        ))}
+      </ul>
+      <form style={inputStyle.form}>
+        <button style={inputStyle.doneButton} type="submit">
+          Done
+        </button>
+        <input
+          style={inputStyle.inputButton}
+          placeholder="Add your To Do here"
+        ></input>
+        <button style={inputStyle.deleteButton} type="submit">
+          Delete
+        </button>
+      </form>
     </>
   );
 }
 
-export default App;
-
-const HeaderStyles = {
-  backgroundColor: "lightblue",
-  display: "flex",
-  justifyContent: "center",
+const listItemStyle = {
+  listContainer: {
+    textAlign: "flex-start",
+  },
+  titleList: {
+    display: "flex",
+    justifyContent: "center",
+    fontFamily: "roboto",
+    fontSize: "40px",
+  },
+  listItem: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "5px",
+    fontFamily: "roboto",
+  },
+  inputTodo: {
+    display: "flex",
+  },
 };
 
-const NavStyle = {
-  display: "flex",
-  listStyle: "none",
-  fontFamily: "roboto",
-  textDecoration: "none",
-  fontSize: "20px",
-};
-
-const NavItemStyle = {
-  margin: "1.5em",
-};
-
-const NavLink = {
-  textDecoration: "none",
-  color: "darkblue",
+const navStyle = {
+  headerContainer: {
+    backgroundColor: "lightblue",
+    display: "flex",
+    justifyContent: "center",
+  },
+  listContainer: {
+    display: "flex",
+    margin: "15px",
+    padding: "10px",
+  },
+  listItems: {
+    listStyle: "none",
+    fontFamily: "roboto",
+    textDecoration: "none",
+    fontSize: "30px",
+    paddingLeft: "40px",
+  },
+  listLink: {
+    textDecoration: "none",
+    color: "darkblue",
+    display: "flex",
+    justifyContent: "center",
+  },
 };
 
 const userAvatarContainer = {
-  fontSize: "20px",
-  textAlign: "center",
-  fontFamily: "fantasy",
-  color: "white",
-  background: "#ff00de",
+  avatarContainer: {
+    fontSize: "50px",
+    textAlign: "center",
+    fontFamily: "fantasy",
+    color: "white",
+    background: "#ff00de",
+  },
+  avatarImage: {
+    margin: "20px",
+    width: "150px",
+    boxShadow: "10px 5px 5px black",
+    borderRadius: "70px",
+  },
 };
 
-const avatar = {
-  margin: "20px",
-  width: "100px",
-  boxShadow: "10px 5px 5px black",
+const inputStyle = {
+  form: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  doneButton: {
+    boxShadow: "10px 5px 5px black",
+    height: "45px",
+    width: "70px",
+    padding: "10px",
+  },
+  deleteButton: {
+    boxShadow: "10px 5px 5x black",
+    height: "45px",
+    width: "70px",
+    padding: "10px",
+  },
+  inputButton: {
+    boxShadow: "10px 5px 5px black",
+    height: "45px",
+    width: "200px",
+    padding: "10px",
+  },
 };
 
-const todoList = {
-  fontFamily: "roboto",
-  textAlign: "center",
-  boxShadow: "10px 5px 5px black",
-  backgroundColor: "lightblue",
-};
-
-const listItemStyle = {
-  listStyle: "none",
-};
-
-const AddToDoButton = {
-  backgroundColor: "fuschia",
-  boxShadow: "10px 5px 5px black",
-  liststyletype: "circle",
-};
-
-const submitButton = {
-  textAlign: "center",
-  boxShadow: "10px 5px 5px black",
-};
-
-const InputButton = {
-  boxShadow: "10px 5px 5px black",
-  height: "45px",
-  width: "300px",
-  padding: "10px",
-};
-
-const InputDeleteButton = {
-  boxShadow: "10px 5px 5px black",
-  height: "45px",
-  width: "70px",
-  padding: "10px",
-};
-
-const InputDoneButton = {
-  boxShadow: "10px 5px 5px black",
-  height: "45px",
-  width: "70px",
-  padding: "10px",
-};
+export default App;
